65b6c4b10856864bcfc5e51df426305443599a81
dwallard <62903783+dwallard@users.noreply.github.com>
2020-11-02 12:31:40 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World</title>
+        
+        <link rel="manifest" href="/manifest.json">
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />  
+        <link rel="apple-touch-icon" href="images/hello-icon-152.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="white"/>
+        <meta name="apple-mobile-web-app-capable" content="yes">  
+        <meta name="apple-mobile-web-app-status-bar-style" content="black"> 
+        <meta name="apple-mobile-web-app-title" content="Hello World"> 
+        <meta name="msapplication-TileImage" content="images/hello-icon-144.png">  
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        
+        
+        
+        <link rel="stylesheet" href="css/nav.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+    
+        <div id="screenFiller">Hidden Place</div>
+    
+        <div class="main">
+            <p>Page Contact</p>   
+        </div>
+        
+    
+        <div class="navbar">
+            <a href="index.php">Home</a>
+            <a href="news.php">News</a>
+            <a href="position.php">Position</a>
+            <a href="contact.php">Contact</a>
+        </div>
+
+
+        <script src="js/main.js"></script>
+    
+    </body>
+</html>
